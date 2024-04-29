715b7d62b6638ecfd371a9ada8f7d01da0dc38ef
tgnlex <alexanderwebber76@gmail.com>
2024-04-28 21:22:01 -0700

    read_csv()
    wrote task for reading csv files

--- src/tasks/core/data.py
+++ src/tasks/core/data.py
@@ -1,7 +1,21 @@
 from celery import shared_task 
+import csv 
 import json
+
 @shared_task 
 def jsonify(object):
   data = json.dumps(object)
   print(data)
   return data
+
+@shared_task
+def read_csv(filename):
+  with open(filename, newline='') as csvfile: 
+    reader = csv.reader(csvfile, delimiter= ' ', quotechar='|')
+    for row in reader:
+      data = []
+      current_row = ', '.join(row)
+      data.append(current_row)
+      print(current_row)
+    return data 
+      
